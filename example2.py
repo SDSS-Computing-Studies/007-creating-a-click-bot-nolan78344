e752479ad27652d7324e01b6bd22ec4beb9ce605
nolano78344 <55156573+nolan78344@users.noreply.github.com>
2021-03-31 10:46:01 -0700

    w

--- example2.py
+++ example2.py
@@ -33,51 +33,6 @@ your screen
 
 """
 
-import pyautogui as p 
-import time
-
-
-#def clicker():
-time.sleep(2)
-i = 0
-buttonloc = p.locateCenterOnScreen("clickthis.png")
-if buttonloc is not None:    
-    print("1This button is at " + str(buttonloc))
-else:
-    print("could not find it")
-    exit()
-
-i = 0
-while True:
-    i = i + 1
-    if i >= 15:       
-        break
-    else:
-        p.click(buttonloc)
-
-#def buyresearch():
-time.sleep(2)
-i = 0
-dataloc = p.locateCenterOnScreen("data1.png", confidence=0.8)
-if dataloc is not None:    
-    print("2This button is at " + str(dataloc))
-else:
-    print("could not find it")
-    exit()
-    
-while True:
-    cantbuyloc = p.locateCenterOnScreen("cantbuy.png")
-    if cantbuyloc is None:
-        break
-    else:
-        i = 0
-        while True:
-            i = i + 1
-            if i >= 5:       
-                break
-            else:
-                p.click(dataloc)
-        
 
 
 #def clicker()
