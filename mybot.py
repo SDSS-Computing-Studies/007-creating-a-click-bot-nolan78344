3f00112d29006cd01df4298058ff1713b6db954b
nolano78344 <55156573+nolan78344@users.noreply.github.com>
2021-04-07 09:42:59 -0700

    Update mybot.py

--- mybot.py
+++ mybot.py
@@ -2,98 +2,238 @@
 import pyautogui as p 
 import time
 
-p.mouseInfo()
+#p.mouseInfo()
 
 def clicker():
-    time.sleep(2)
+    time.sleep(1)
     i = 0
     buttonloc = p.locateCenterOnScreen("clickthis.png")
     if buttonloc is not None:    
         print("1This button is at " + str(buttonloc))
     else:
-        print("could not find it")
-        exit()
+        print("clicker1 could not find it")
 
     i = 0
     while True:
         i = i + 1
-        if i >= 15:       
-            break
+        if i >= 76:       
+            return
         else:
             p.click(buttonloc)
 
-def buyresearch():
-    time.sleep(2)
+def buyresearch1():
     i = 0
-    dataloc = p.locateCenterOnScreen("data1.png", confidence=0.8, region=(104,260, 100, 150))
+    dataloc = p.click(x=100, y=232)
     if dataloc is not None:    
         print("2This button is at " + str(dataloc))
     else:
-        print("could not find it")
-        exit()
-    
+        print("2could not find it")
     while True:
-        cantbuydataloc = p.locateCenterOnScreen("cantbuy.png", region=(104,260, 100, 100))
+        cantbuydataloc = p.locateCenterOnScreen("cantbuy.png")
         if cantbuydataloc is None:
-            break
+            return
         else:
             i = 0
             while True:
                 i = i + 1
                 if i >= 5:       
-                    break
+                    return
                 else:
                     p.click(dataloc)
 
-def buyhr():
-    time.sleep(2)
+def buyhr1():
     i = 0
-    hrloc = p.locateCenterOnScreen("hr40buy.jpg", confidence=0.8, region=(1313,313, 50, 150))
+    hrloc = p.click(x=1327, y=239)
     if hrloc is not None:    
         print("3This button is at " + str(hrloc))
     else:
-        print("could not find it")
-        exit()
-    
+        print("3could not find it")
     while True:
-        cantbuyhrloc = p.locateCenterOnScreen("cant40buy.jpg", region=(1313,313, 50, 50))
+        cantbuyhrloc = p.locateCenterOnScreen("cant40buy.jpg")
         if cantbuyhrloc is None:
-            break
+            return
         else:
             i = 0
             while True:
                 i = i + 1
                 if i >= 5:       
-                    break
+                    return
                 else:
                     p.click(hrloc)
 
 def buyupgrades():
-    time.sleep(2)
     i = 0
-    upgradeloc = p.locateCenterOnScreen("buyupgrades.jpg", confidence=0.8, region=(1658,294, 50, 150))
+    upgradeloc = p.click(x=1657, y=299)
     if upgradeloc is not None:    
         print("4This button is at " + str(upgradeloc))
     else:
         print("could not find it")
-        exit()
     
     while True:
-        cantbuyupgradeloc = p.locateCenterOnScreen("cantbuyupgrade.jpg", region=(1658,294, 50, 50))
+        cantbuyupgradeloc = p.locateCenterOnScreen("cantbuyupgrade.jpg")
         if cantbuyupgradeloc is None:
-            break
+            return
         else:
             i = 0
             while True:
                 i = i + 1
                 if i >= 5:       
-                    break
+                    return
                 else:
                     p.click(upgradeloc)
-a = 0
-while (a<10):
-    a = a + 0
-    def clicker():
-        def buyresearch():
-            def buyhr():
-                def buyupgrades():
+
+def buyresearch2():
+    i = 0
+    dataloc = p.click(x=95, y=297)
+    if dataloc is not None:    
+        print("2This button is at " + str(dataloc))
+    else:
+        print("could not find it")
+    
+    while True:
+        cantbuydataloc = p.locateCenterOnScreen("cantbuy.png")
+        if cantbuydataloc is None:
+            return
+        else:
+            i = 0
+            while True:
+                i = i + 1
+                if i >= 5:       
+                    return
+                else:
+                    p.click(dataloc)
+
+def buyhr2():
+    i = 0
+    hrloc = p.click(x=1339, y=316)
+    if hrloc is not None:    
+        print("3This button is at " + str(hrloc))
+    else:
+        print("could not find it")
+    
+    while True:
+        cantbuyhrloc = p.locateCenterOnScreen("cant40buy.jpg")
+        if cantbuyhrloc is None:
+            return
+        else:
+            i = 0
+            while True:
+                i = i + 1
+                if i >= 5:       
+                    return
+                else:
+                    p.click(hrloc)
+
+def buyresearch3():
+    i = 0
+    dataloc = p.click(x=88, y=369)
+    if dataloc is not None:    
+        print("2This button is at " + str(dataloc))
+    else:
+        print("could not find it")
+    
+    while True:
+        cantbuydataloc = p.locateCenterOnScreen("cantbuy.png")
+        if cantbuydataloc is None:
+            return
+        else:
+            i = 0
+            while True:
+                i = i + 1
+                if i >= 5:       
+                    return
+                else:
+                    p.click(dataloc)
+
+def buyhr3():
+    i = 0
+    hrloc = p.click(x=1339, y=380)
+    if hrloc is not None:    
+        print("3This button is at " + str(hrloc))
+    else:
+        print("could not find it")
+    
+    while True:
+        cantbuyhrloc = p.locateCenterOnScreen("cant40buy.jpg")
+        if cantbuyhrloc is None:
+            return
+        else:
+            i = 0
+            while True:
+                i = i + 1
+                if i >= 5:       
+                    return
+                else:
+                    p.click(hrloc)
+
+def buyresearch4():
+    i = 0
+    dataloc = p.click(x=96, y=447)
+    if dataloc is not None:    
+        print("2This button is at " + str(dataloc))
+    else:
+        print("4could not find it")
+    
+    while True:
+        cantbuydataloc = p.locateCenterOnScreen("cantbuy.png")
+        if cantbuydataloc is None:
+            return
+        else:
+            i = 0
+            while True:
+                i = i + 1
+                if i >= 5:       
+                    return
+                else:
+                    p.click(dataloc)
+
+
+def buyhr4():
+    i = 0
+    hrloc = p.click(x=1339, y=467)
+    if hrloc is not None:    
+        print("3This button is at " + str(hrloc))
+    else:
+        print("could not find it")
+    
+    while True:
+        cantbuyhrloc = p.locateCenterOnScreen("cant40buy.jpg")
+        if cantbuyhrloc is None:
+            return
+        else:
+            i = 0
+            while True:
+                i = i + 1
+                if i >= 5:       
+                    return
+                else:
+                    p.click(hrloc)
+
+def clicker2():
+    i = 0
+    buttonloc = p.locateCenterOnScreen("clickthis.png")
+    if buttonloc is not None:    
+        print("1This button is at " + str(buttonloc))
+    else:
+        print("clicker2 could not find it")
+
+    i = 0
+    while True:
+        i = i + 1
+        if i >= 101:       
+            return
+        else:
+            p.click(buttonloc)
+
+while True:
+    clicker()
+    buyresearch1()
+    buyhr1()
+    buyupgrades()
+    buyresearch2()
+    buyhr2()
+    buyresearch3()
+    buyhr3()
+    buyresearch4()
+    buyhr4()
+    clicker2()
